1328b5b52fdb751d2ec2ba477c23b17522abfedf
abdullah haidary <abdullahhaidary4262@gmail.com>
2025-03-18 19:22:11 +0430

    feat: added the TrainStation apiResource

--- app/Http/Controllers/Api/TrainStationController.php
+++ app/Http/Controllers/Api/TrainStationController.php
@@ -3,9 +3,45 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\TrainStation;
+use App\Services\TrainStationService;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class TrainStationController extends Controller
 {
-    //
+    protected $trainStationService;
+
+    public function __construct(TrainStationService $trainStationService)
+    {
+        $this->trainStationService = $trainStationService;
+    }
+
+    public function index()
+    {
+        return response()->json($this->trainStationService->getAll());
+    }
+
+    public function store(Request $request)
+    {
+        $trainStation = $this->trainStationService->create($request->all());
+        return response()->json($trainStation, Response::HTTP_CREATED);
+    }
+
+    public function show(TrainStation $trainStation)
+    {
+        return response()->json($trainStation);
+    }
+
+    public function update(Request $request, TrainStation $trainStation)
+    {
+        $updatedStation = $this->trainStationService->update($trainStation, $request->all());
+        return response()->json($updatedStation);
+    }
+
+    public function destroy(TrainStation $trainStation)
+    {
+        $this->trainStationService->delete($trainStation);
+        return response()->json(null, Response::HTTP_NO_CONTENT);
+    }
 }
--- app/services/TrainStationService.php
+++ app/services/TrainStationService.php
@@ -5,5 +5,24 @@ use App\Models\TrainStation;
 
 class TrainStationService
 {
-    
+    public function getAll()
+    {
+        return TrainStation::latest()->get();
+    }
+
+    public function create(array $data)
+    {
+        return TrainStation::create($data);
+    }
+
+    public function update(TrainStation $trainStation, array $data)
+    {
+        $trainStation->update($data);
+        return $trainStation;
+    }
+
+    public function delete(TrainStation $trainStation)
+    {
+        return $trainStation->delete();
+    }
 }
