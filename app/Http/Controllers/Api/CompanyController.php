fb8971d4c5b19c26ea04c680a934b6cd68909a04
abdullah haidary <abdullahhaidary4262@gmail.com>
2025-03-18 19:42:53 +0430

    feat: implement CompanyController with CRUD operations and validation using CompanyRequest

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -3,9 +3,46 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Company;
+use App\Services\CompanyService;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Requests\CompanyRequest;
 
 class CompanyController extends Controller
 {
-    //
+    protected $companyService;
+
+    public function __construct(CompanyService $companyService)
+    {
+        $this->companyService = $companyService;
+    }
+
+    public function index()
+    {
+        return response()->json($this->companyService->getAll());
+    }
+
+    public function store(CompanyRequest $request)
+    {
+        $company = $this->companyService->create($request->validated());
+        return response()->json($company, Response::HTTP_CREATED);
+    }
+
+    public function show(Company $company)
+    {
+        return response()->json($company);
+    }
+
+    public function update(CompanyRequest $request, Company $company)
+    {
+        $updatedCompany = $this->companyService->update($company, $request->validated());
+        return response()->json($updatedCompany);
+    }
+
+    public function destroy(Company $company)
+    {
+        $this->companyService->delete($company);
+        return response()->json(null, Response::HTTP_NO_CONTENT);
+    }
 }
