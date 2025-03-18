660733061dde99549b0504dcfa0b541e3e1e945a
abdulwaliaryan7 <abdulwaliaryan897@gmail.com>
2025-03-18 11:25:13 +0430

    the countrey, company, transport_types, products_type, product and train stations model, migration,controller and seader created

---
+++ database/migrations/2025_03_18_060717_create_train_stations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('train_stations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transport_type_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('product_type_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('company_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('product_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('country_from_id')->nullable()->constrained('countries')->nullOnDelete();
+            $table->foreignId('country_to_id')->nullable()->constrained('countries')->nullOnDelete();
+            $table->integer('number_of_wagon');
+            $table->string('weight');
+            $table->integer('number_of_bar');
+            $table->date('date_of_out');
+            $table->date('date_of_in');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('train_stations');
+    }
+};
