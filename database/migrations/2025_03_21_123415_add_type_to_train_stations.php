0e240212eb0e3ab63041939db22f39bdc3304b55
Farhad Rahmani <55535348+farhadrahmanii@users.noreply.github.com>
2025-03-22 16:02:34 +0430

    Resouces is created

--- database/migrations/2025_03_21_123415_add_type_to_train_stations.php
+++ database/migrations/2025_03_21_123415_add_type_to_train_stations.php
@@ -12,7 +12,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('train_stations', function (Blueprint $table) {
-            $table->enum('type', Type::values())->nullable();
+            // $table->enum('type', Type::values())->nullable();
             // $table->enum('product_type', ProductType::values())->nullable();
         });
     }
